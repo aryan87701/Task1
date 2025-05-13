c3ee678a6c6d19ed1ab85abbc98fc88a85a39c6e
aryan87701 <singharyan792005@gmail.com>
2025-05-13 18:03:48 +0530

    submitc

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import Dutch from './../src/assets/Dutch.png'
 import Card from '../src/Components/Styled';
 function Contact(){
+const handleSubmit = (e) =>{
+   e.preventDefault()
+   alert('Your message was successfully sent')
+}
   return (
     <div className='bg'>
       <div className='dutch'>
         <img src={Dutch} alt="" className='z-2'/>
       </div>
     
-      <div className='formContainer'>
+      <div className='formContainer' >
         <h1 className='text-center my-2'>CONTACT US !</h1>
-        <form className='flex flex-col'>
+        <form className='flex flex-col ' onSubmit={handleSubmit}>
           Name : <input type="text" required className='in' placeholder='Enter your Name'/>
           Email : <input type="email" required className='in' placeholder='example@gmail.com'/>
           Message : <textarea name="" id="" className='in' placeholder='Your Message to us'></textarea>
-          <button onClick={(e)=>{
-            e.preventDefault()
-            alert('Your Message is Successfully sent')
-          }}>SUBMIT</button>
+          <button type='submit'>SUBMIT</button>
         </form>
       </div>
       <div className='CCard'>
